4795132560a700be7abdc84294c495d2e5aca9bc
Michael Monkenbusch(AB) <m.monkenbusch@fz-juelich.de>
2016-11-15 21:40:43 +0100

    th template generator

--- src/UTILITIES/th_template_generator.f90
+++ src/UTILITIES/th_template_generator.f90
@@ -15,16 +15,16 @@ implicit none
  integer             :: noutpar
 
  
- character(len=namelen)    :: theoryname
- character(len=4*commlen)  :: theory_description 
- character(len=commlen)    :: theory_citation
- character(len=namelen)    :: parametername(mthpar)
- character(len=commlen)    :: parameter_description(mthpar)
- character(len=namelen)    :: recparin(mrecpar)
- character(len=commlen)    :: recparin_default(mrecpar)
- character(len=commlen)    :: recparin_description(mrecpar)
- character(len=namelen)    :: recparout(mrecpar)
- character(len=commlen)    :: recparout_description(moutpar)
+ character(len=namelen)    :: theoryname                      =" "
+ character(len=4*commlen)  :: theory_description              =" "
+ character(len=commlen)    :: theory_citation                 =" "
+ character(len=namelen)    :: parametername(mthpar)           =" "
+ character(len=commlen)    :: parameter_description(mthpar)   =" "
+ character(len=namelen)    :: recparin(mrecpar)               =" "
+ character(len=commlen)    :: recparin_default(mrecpar)       =" "
+ character(len=commlen)    :: recparin_description(mrecpar)   =" "
+ character(len=namelen)    :: recparout(mrecpar)              =" "
+ character(len=commlen)    :: recparout_description(moutpar)  =" "
 
  character(len=commlen+namelen+1) :: inline, buf
 
@@ -43,7 +43,7 @@ implicit none
  
  ! read until keyword THEORY
  d1: do 
-       read(in,'(a)',end=999,err=999) inline
+       read(in,'(a)',end=999,err=999) buf; inline = adjustl(buf)
        i = index(inline,"#THEORY ")
        if( index(inline,"#THEORY ") > 0) then
          theoryname = adjustl(trim(inline(i+7:len_trim(inline))))
@@ -57,7 +57,7 @@ implicit none
  !
     theory_description = " "
 d2: do
-       read(in,'(a)',end=999,err=999) inline
+       read(in,'(a)',end=999,err=999)  buf; inline = adjustl(buf)
        i = index(inline,"#CITE")
        if(i>0) exit d2
        theory_description = trim(theory_description)//" "//trim(inline)
@@ -66,7 +66,7 @@ d2: do
     write(6,*)"thdesc: ",trim(theory_description)
 
 d2a: do
-       read(in,'(a)',end=999,err=999) inline
+       read(in,'(a)',end=999,err=999)   buf; inline = adjustl(buf)
        i = index(inline,"#PARAMETERS")
        if(i>0) exit d2a
        theory_citation = trim(theory_citation)//" "//trim(inline)
@@ -78,23 +78,24 @@ d2a: do
 !
     npar = 0
 d3: do
-       read(in,'(a)',end=999,err=999) inline
+       read(in,'(a)',end=999,err=999)  buf; inline = adjustl(buf)
        i = index(inline,"#RECIN-PARAMETERS")
        if(i>0) exit d3
        npar = npar+1
        buf = adjustl(trim(inline))
-       parametername(npar)         = buf(1:8)
-       parameter_description(npar) = adjustl(trim(buf(9:len(buf))))  
+       j=index(buf,"!")
+       if(j==0) j=len_trim(buf)+1
+       parametername(npar)         = buf(1:j-1)
+       parameter_description(npar) = adjustl(trim(buf(j+1:len(buf))))  
        write(6,'(i3,a,3x,a)') npar,  parametername(npar),  trim(parameter_description(npar))    
     enddo d3
-    npar = 0
 
 !
 ! read now the parameters until recoutparameters
 !
     nrecpar = 0
 d4: do
-       read(in,'(a)',end=999,err=999) inline
+       read(in,'(a)',end=999,err=999)  buf; inline = adjustl(buf)
        i = index(inline,"#RECOUT-PARAMETERS")
        if(i>0) exit d4
        nrecpar = nrecpar+1
@@ -111,14 +112,16 @@ d4: do
 !
     noutpar = 0
 d5: do
-       read(in,'(a)',end=999,err=999) inline
+       read(in,'(a)',end=999,err=999)  buf; inline = adjustl(buf)
        i = index(inline,"#VARIABLES")
        if(i>0) exit d5
        noutpar = noutpar+1
        buf = adjustl(trim(inline))
        recparout(noutpar)         = buf(1:8)
-       recparout_description(npar) = adjustl(trim(buf(9:)))  
-       write(6,'(i5," << ",a,3x,a)') noutpar,  recparin(noutpar),  trim(recparin_description(noutpar))    
+       j=index(buf,"!")
+       if(j==0) j=len_trim(buf)+1
+       recparout_description(noutpar) = adjustl(trim(buf(j+1:)))  
+       write(6,'(i5," << ",a,3x,a)') noutpar,  recparout(noutpar),  trim(recparout_description(noutpar))    
     enddo d5
 
 !
@@ -149,15 +152,21 @@ d7: do
       write(out,'(a)') trim(inline)
     enddo d7
 
-   write(out,'(3a)')"      th_",trim(adjustl(theoryname))," = #RESULT"
+   write(out,'(3a)')"     th_",trim(adjustl(theoryname))," = ! INSERT RESULTING VALUE OF TH EVALUATION HERE"
+   write(out,'(a)')" "
+   write(out,'(a)')"! ---- writing computed parameters to the record >>>  "
 o1: do i=1,noutpar
-      write(out,'()')"      call parset('",recparout(i),"',sngl(",recparout(i),"),ier)"
+      write(out,'(5a)')"      call parset('",recparout(i),"',sngl(",trim(recparout(i)),"),iadda,ier)"
     enddo o1
 
 !
 ! read now 1 to 1 subroutines impelemns
 !
+  write(out,'(a)')" "
   write(out,'(a)')" CONTAINS "
+  write(out,'(a)')" "
+  write(out,'(a)')"! subroutines and functions entered here are private to this theory and share its variables "
+  write(out,'(a)')" "
   
 d8: do
       read(in,'(a)',end=999,err=999) inline
@@ -170,6 +179,9 @@ d8: do
 
  
  close(out)
+
+ write(6,*)
+ write(6,*)"The theory template is written to out.f90, complete it by editing and copy to th_",trim(theoryname),".f90"
  stop
 999 continue
  write(6,*)"ERROR: premature end"
@@ -187,30 +199,30 @@ CONTAINS
    write(out,'(a)')    "!===================================================================================="
    write(out,'(8("! ",128a))') trim(theory_description)
    write(out,'(2("! ",128a))') trim(theory_citation)
-   write(out,'(a)') "  use theory_description "
-   write(out,'(a)') "  implicit none "
-   write(out,'(a)') "  character(len=8) :: thnam, parnam (*) "
-   write(out,'(a)') "  real    :: pa (*) "
-   write(out,'(a)') "  real    :: x "
-   write(out,'(a)') "  integer :: mbuf, nparx, ier, ini, npar, iadda"
-   write(out,'(a)') "  integer, intent(inout) :: nopar       "                ! Anzahl der Parameter data
-   write(out,'(a)') "  character(len=80), intent(inout) :: napar(mbuf) "      ! name des parameters n
-   write(out,'(a)') "  real, intent(inout) :: params(mbuf) "                  ! value des parameters n
-   write(out,'(a)') " "
-   write(out,'(a)') "  double precision, parameter :: Pi = 4*atan(1d0)"
-   write(out,'(a)') "  integer                     :: actual_record_address"
-   write(out,'(a)') " "
-   write(out,'(a)') "! the internal parameter representation "
+   write(out,'(4x,a)') "  use theory_description "
+   write(out,'(4x,a)') "  implicit none "
+   write(out,'(4x,a)') "  character(len=8) :: thnam, parnam (*) "
+   write(out,'(4x,a)') "  real    :: pa (*) "
+   write(out,'(4x,a)') "  real    :: x , xh"
+   write(out,'(4x,a)') "  integer :: mbuf, nparx, ier, ini, npar, iadda"
+   write(out,'(4x,a)') "  integer, intent(inout) :: nopar       "                ! Anzahl der Parameter data
+   write(out,'(4x,a)') "  character(len=80), intent(inout) :: napar(mbuf) "      ! name des parameters n
+   write(out,'(4x,a)') "  real, intent(inout) :: params(mbuf) "                  ! value des parameters n
+   write(out,'(4x,a)') " "
+   write(out,'(4x,a)') "  double precision, parameter :: Pi = 4*atan(1d0)"
+   write(out,'(4x,a)') "  integer                     :: actual_record_address"
+   write(out,'(4x,a)') " "
+   write(out,'(a)'   ) "! the internal parameter representation "
    do i=1,npar
-      write(out,'(a,a,a,a)') "  double precision :: ",parametername(npar),"   ! ",parameter_description(i)(1:80)
+      write(out,'(5x,a,a,a,a)') "double precision :: ",parametername(i),"   ! ",parameter_description(i)(1:80)
    enddo
    write(out,'(a)') "! the recin parameter representation "
    do i=1,nrecpar
-      write(out,'(a,a,a,a)') "  double precision :: ",recparin(npar),"   ! ",recparin_description(i)(1:80)
+      write(out,'(5x,a,a,a,a)') "double precision :: ",recparin(i),"   ! ",recparin_description(i)(1:80)
    enddo
    write(out,'(a)') "! the reout parameter representation "
    do i=1,noutpar
-      write(out,'(a,a,a,a)') "  double precision :: ",recparout(npar),"   ! ",recparout_description(i)(1:80)
+      write(out,'(5x,a,a,a,a)') "double precision :: ",recparout(i),"   ! ",recparout_description(i)(1:80)
    enddo
    write(out,'(a)') " "
  
@@ -225,22 +237,22 @@ CONTAINS
     integer :: i
       write(out,'(a)')     "!"
       write(out,'(a)')     "! ----- initialisation ----- "
-      write(out,'(a)')     "      IF (ini.eq.0) then     "
-      write(out,'(a,a,a)') "         thnam = '",trim(adjustl(theoryname)),"'"
-      write(out,'(a,i8)')  "         nparx = ",npar
-      write(out,'(a)')     "         IF (npar.lt.nparx) then"
+      write(out,'(a)')     "    IF (ini.eq.0) then     "
+      write(out,'(a,a,a)') "       thnam = '",trim(adjustl(theoryname)),"'"
+      write(out,'(a,i8)')  "       nparx = ",npar
+      write(out,'(a)')     "       IF (npar.lt.nparx) then"
       write(out,'(a)')     &
-      "            WRITE (6,(' theory: ',a8,' no of parametrs=',i8,' exceeds current max. = ',i8))thnam,nparx,npar"
+      "          WRITE (6,(' theory: ',a8,' no of parametrs=',i8,' exceeds current max. = ',i8))thnam,nparx,npar"
       write(out,'(a,a,a)') "            th_",trim(adjustl(theoryname))," = 0" 
-      write(out,'(a)')     "            RETURN"
-      write(out,'(a)')     "         ENDIF"
-      write(out,'(a)')     "         npar = nparx"
+      write(out,'(a)')     "          RETURN"
+      write(out,'(a)')     "       ENDIF"
+      write(out,'(a)')     "       npar = nparx"
       write(out,'(a)')     "! >>>>> describe theory with >>>>>>> "
-      write(out,'(a)')     "        idesc = next_th_desc()"
-      write(out,'(a)')     "        th_identifier(idesc)   = thnam"
-      write(out,'(4a)')    "        th_explanation(idesc)  = ",'"',trim(theory_description),'"'
-      write(out,'(4a)')    "        th_citation(idesc) = ",'"',trim(theory_citation),'"'
-      write(out,'(a)')     "!        --------------> set the parameter names --->"
+      write(out,'(a)')     "       idesc = next_th_desc()"
+      write(out,'(a)')     "       th_identifier(idesc)   = thnam"
+      write(out,'(4a)')    "       th_explanation(idesc)  = ",'"',trim(theory_description),'"'
+      write(out,'(4a)')    "       th_citation(idesc)     = ",'"',trim(theory_citation),'"'
+      write(out,'(a)')     "!       --------------> set the parameter names --->"
   
 l1:   do i=1,npar
        write(out,'(a,i2,a,a,a,a)') "        parnam (",i,") = '",parametername(i),"'  ! ",parameter_description(i)(1:80)
@@ -250,37 +262,44 @@ l2:   do i=1,npar
         write(out,'(a,i2,3a)')     '        th_param_desc(',i,',idesc) = "',trim(parameter_description(i)),'" !//cr//parspace//&'
       enddo l2
       write(out,'(a)')     "! >>>>> describe record parameters used >>>>>>>"
-      write(out,'(a)')     '       th_file_param(:,idesc) = " " '
+      write(out,'(a)')     '        th_file_param(:,idesc) = " " '
 l3:   do i=1,nrecpar
-       write(out,'(a,i3,5a)')    '       th_file_param(',i,',idesc) = "',recparin(i),' > ',trim(recparin_description(i)),'"'
+       write(out,'(a,i3,5a)')    '        th_file_param(',i,',idesc) = "',recparin(i),' > ',trim(recparin_description(i)),'"'
       enddo l3
       write(out,'(a)')     "! >>>>> describe record parameters creaqted by this theory >>>>>>> "
-      write(out,'(a)')     '       th_out_param(:,idesc)  = " "'  
+      write(out,'(a)')     '        th_out_param(:,idesc)  = " "'  
 l4:   do i=1,noutpar
-       write(out,'(a,i3,5a)')    '       th_out_param(',i,',idesc) = "',recparout(i),' > ',trim(recparout_description(i)),'"'
+       write(out,'(a,i3,5a)')    '        th_out_param(',i,',idesc) = "',recparout(i),' > ',trim(recparout_description(i)),'"'
       enddo l4
       write(out,'(a)')     "! "
-      write(out,'(3a)')    "         th_",trim(adjustl(theoryname))," = 0.0"
+      write(out,'(3a)')    "        th_",trim(adjustl(theoryname))," = 0.0"
       write(out,'(a)')     " "
-      write(out,'(a)')     "         RETURN"
-      write(out,'(a)')     "      ENDIF"
+      write(out,'(a)')     "        RETURN"
+      write(out,'(a)')     "     ENDIF"
       write(out,'(a)')     "!"
       write(out,'(a)')     "! ---- transfer parameters -----"
 
 l5:   do i=1,npar
-        write(out,'(a,a,i2,a)') parametername(i)," =      pa(",i,")" 
+        write(out,'(6x,a,a,i2,a)') parametername(i)," =      pa(",i,")" 
       enddo l5
 
-      write(out,'(a)')     "! ---- extract -----"
+      write(out,'(a)') "! ---- extract parameters that are contained in the present record under consideration by fit or thc ---"
       write(out,'(a)')     "      iadda = actual_record_address()"
 l6:   do i=1,nrecpar
-        write(out,'(4a)')  "     ",recparin(i)," = ",recparin_default(i)
-        write(out,'(4a)')  "      call parget('",recparin(i),"',",recparin(i),",iadda,ier)"
+        write(out,'(2a)')  "! >>> extract: ",trim(recparin_description(i))
+        write(out,'(2a)')  "      xh = ",trim(recparin_default(i))
+        write(out,'(5a)')  "      call parget('",recparin(i),"',xh,iadda,ier)"
+        write(out,'(3a)')  "      ",recparin(i)," = xh"
       enddo l6
 
 
 
-      write(out,'(a)')     "! ----------------- implementation ------------------"
+      write(out,'(a)')     "! "
+      write(out,'(a)')     "! ------------------------------------------------------------------"
+      write(out,'(a)')     "! ----------------------- implementation ---------------------------"
+      write(out,'(a)')     "! ------------------------------------------------------------------"
+      write(out,'(a)')     "! "
+
 
 
   end subroutine make_init_section
