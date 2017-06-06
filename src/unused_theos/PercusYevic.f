6ce59a3fa71f532b488c752dd871b6008a242084
Michael Monkenbusch(AB) <m.monkenbusch@fz-juelich.de>
2017-06-06 10:30:39 +0200

    hetdif restored

---
+++ src/unused_theos/PercusYevic.f
@@ -0,0 +1,38 @@
+ 
+c  percus-yevick s(q)
+c -------------------
+c lit. m.s. wertheim, prl 10 (1963) 321
+c -------------------------------------
+c
+       function peryev(q,rr,den,eps)
+c      -----------------------------
+c      q     = impulsuebertrag
+c      rr    = teilchenradius
+c      den   = teilchendichte
+c      eps   = realteil ---> 0
+c
+       parameter(pi=3.141592654)
+       implicit complex*16 (c)
+       complex*16           dcmplx, cdexp
+       real*8   eta, dimag, deps, dr, dq, q, r, rr, den, eps
+       real*8   peryev
+c
+       r   = 2*rr
+c --- r ist der durchmesser ! der teilchen !
+       dr  = r
+       dq  = q
+       deps= eps
+       eta = pi*den*r*r*r/6.d0
+c
+       ct  = dcmplx ( deps , -dq*dr )
+       cst = (((1.d0-eta)**2*ct+6.d0*eta*(1.d0-eta))*ct
+     *      +18.d0*eta*eta)*ct
+     *      -12.d0*eta*(1.d0+2.d0*eta)
+       clt = 12.d0*eta*((1.d0+0.5d0*eta)*ct+(1.d0+2.d0*eta))
+c
+       cgt = (ct*clt) / ( 12.d0*eta*(clt+cst*cdexp(ct)) )
+c
+       peryev =  1.d0 + den * (4.d0*pi/dq) * dr*dr * dimag( cgt )
+c
+       return
+       end
