de134d845e7edd2191bd44cd023685c60a0380f1
Monkenbusch, Michael <m.monkenbusch@fz-juelich.de>
2022-03-31 10:26:11 +0200

    new_com table utility

--- src/new_com.F90
+++ src/new_com.F90
@@ -1,3 +1,330 @@
+!! Collection of string utilities that may be useful
+
+module strings_module
+  implicit none
+
+  private
+
+  public :: split, tolower, toupper, match_wild
+  public :: replace_char, replace_str
+  public :: string_replace ! function
+
+contains
+
+  ! -------------------------------------------------------------------
+  !>
+  function tolower(str) result(lowercase)
+    character(len=*), intent(in) :: str
+    character(len=len(str)) :: lowercase
+    integer :: i
+    do i = 1, len(str)
+       select case(str(i:i))
+       case("A":"Z")
+          lowercase(i:i) = achar(iachar(str(i:i))+32)
+       case default
+          lowercase(i:i) = str(i:i)
+       end select
+    end do
+  end function tolower
+
+  ! -------------------------------------------------------------------
+  !>
+  function toupper(str) result(uppercase)
+    character(len=*), intent(in) :: str
+    character(len=len(str)) :: uppercase
+    integer :: i
+    do i = 1, len(str)
+       select case(str(i:i))
+       case("a":"z")
+          uppercase(i:i) = achar(iachar(str(i:i))-32)
+       case default
+          uppercase(i:i) = str(i:i)
+       end select
+    end do
+  end function toupper
+
+
+  ! -------------------------------------------------------------------
+  !> split a string
+  subroutine split(string, separator, words, nwords, max_len, max_words)
+    integer, intent(in) :: max_len
+    integer, intent(in) :: max_words
+    character(len=*), intent(in) :: string
+    character(len=*), intent(in) :: separator
+    character(len=max_len), dimension(max_words), intent(out) :: words
+    integer, intent(inout) :: nwords
+    !
+    integer :: pos1, pos2, str_len
+
+    nwords  = 0
+    pos1    = 1
+    pos2    = 0
+    str_len = len_trim(string)
+    do
+       if ( nwords > max_words ) return
+       pos2 = index(string(pos1: ), separator)
+       if ( pos2 <= 0 ) then
+          if ( len_trim(string(pos1:))>0 ) then
+             nwords = nwords + 1
+             words(nwords) = string(pos1:)
+          endif
+          return
+       end if
+       if ( len_trim(string(pos1:pos1+pos2-2))>0 ) then
+          nwords = nwords + 1
+          words(nwords) = string(pos1:pos1+pos2-2)
+       endif
+       pos1 = pos2 + pos1
+       if ( pos1 > str_len ) return
+    end do
+  end subroutine split
+
+
+  ! -------------------------------------------------------------------
+  !> string replace function
+  !! replace one of the characters from oldchars by a newchar character
+  subroutine replace_char(string,oldchars,newchar)
+    character(len=*), intent(inout) :: string
+    character(len=*), intent(in)    :: oldchars
+    character(len=1), intent(in)    :: newchar
+    !
+    integer   :: pos1, pos2, slen
+    integer   :: i
+
+    slen = len_trim(string)
+    do i=1, len_trim(oldchars)
+       pos1 = 1
+       pos2 = 0
+       do
+          pos2 = index(string(pos1:),oldchars(i:i))
+          if ( pos2<=0    ) exit
+          pos1 = pos1 + pos2 - 1
+          if ( pos1>slen ) exit
+          string(pos1:pos1) = newchar
+       end do
+    end do
+  end subroutine replace_char
+
+  ! -------------------------------------------------------------------
+  !> replace substring by a new string
+  subroutine replace_str(string,substr,newstring)
+    character(len=*), intent(inout) :: string
+    character(len=*), intent(in)    :: substr
+    character(len=*), intent(in)    :: newstring
+    !
+    integer   :: pos1, slen, sublen, cadd, mlen
+
+    sublen  = len_trim(substr)
+    cadd    = len_trim(newstring)-sublen
+    mlen    = len(string)
+
+    lp: do
+        pos1    = index(string, trim(substr))
+        if(pos1 <= 0 )       return
+        slen    = len_trim(string)
+        if(slen+cadd > mlen) return
+        string  = string(1:pos1-1)//trim(newstring)//string(pos1+sublen:)
+    enddo lp
+  end subroutine replace_str
+
+
+  ! =========================================================================
+  !> another one, this time it is a function and trims empty space in between
+  function string_replace(string, substring1, substring2) result(new_string)
+    character(len=*), intent(in)         :: string
+    character(len=*), intent(in)         :: substring1
+    character(len=*), intent(in)         :: substring2
+    character(len=len(string)+(len(string)/len(substring1)+1)*len(substring2)) :: new_string
+    integer  :: i, k, l, i1, i2, len0, len1, len2
+
+    l    = len(string)
+    len0 = len_trim(string)
+    len1 = len_trim(substring1)
+    len2 = len_trim(substring2)
+
+    new_string = ' '
+    i1 = 1
+    i2 = l
+
+    do k=1,l
+        i = index(string(i1:l),substring1(1:len1))
+        if(i == 0) then
+            new_string = trim(new_string)//string(i1:l)
+            exit
+        endif
+
+        i = i + i1 - 1
+        new_string = trim(new_string)//string(i1:i-1)//substring2(1:len2)
+        i1 = i+len1
+    enddo
+
+    new_string = trim(new_string)
+    return
+  end function string_replace
+
+
+
+
+  ! -------------------------------------------------------------------
+  !>
+  !> Compare given string for match to pattern which may
+  !! contain wildcard characters:
+  !! - "?" matching any one character, and
+  !! - "*" matching any zero or more characters.
+  !! Both strings may have trailing spaces which are ignored.
+  !!
+  !! @authors
+  !!      Clive Page, userid: cgp  domain: le.ac.uk, 2003 (original code)
+  !!      Rolf Sander, 2005 (bug fixes and pattern preprocessing)
+  !! @remarks
+  !! Minor bug fixed by Clive Page, 2005 Nov 29, bad comment fixed 2005 Dec 2.
+  !! Serious bug fixed by  Robert H McClanahan, 2011 April 11th
+  !
+  !    This program is free software; you can redistribute it and/or modify
+  !    it under the terms of the GNU General Public License as published by
+  !    the Free Software Foundation; either version 2 of the License, or
+  !    (at your option) any later version.
+  !
+  !    This program is distributed in the hope that it will be useful,
+  !    but WITHOUT ANY WARRANTY; without even the implied warranty of
+  !    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  !    GNU General Public License for more details.
+  !
+  !    You should have received a copy of the GNU General Public License
+  !    along with this program; if not, write to the Free Software
+  !    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+  !    02110-1301  USA
+  !
+  logical function match_wild (pattern, string)
+    character(len=*), intent(in) :: pattern ! pattern may contain * and ?
+    character(len=*), intent(in) :: string  ! string to be compared
+    integer :: lenp, lenp2, lens, n, p2, p, s
+    integer :: n_question, n_asterisk
+    logical :: found
+
+    character(len=len(pattern)) :: pattern2
+    lens = len_trim(string)
+    lenp = len_trim(pattern)
+
+    ! if the pattern is empty, always return true
+    IF (lenp == 0) THEN
+       match_wild = .TRUE.
+       RETURN
+    ENDIF
+
+    ! The pattern must be preprocessed. All consecutive occurrences of
+    ! one or more question marks ('?') and asterisks ('*') are sorted and
+    ! compressed. The result is stored in pattern2.
+
+    pattern2(:)=''
+    p  = 1 ! current position in pattern
+    p2 = 1 ! current position in pattern2
+    DO
+       IF ((pattern(p:p) == '?').OR.(pattern(p:p) == '*')) THEN
+          ! a special character was found in the pattern
+          n_question = 0
+          n_asterisk = 0
+          DO WHILE (p <= lenp)
+             ! count the consecutive question marks and asterisks
+             IF ((pattern(p:p) /= '?').AND.(pattern(p:p) /= '*')) EXIT
+             IF (pattern(p:p) == '?') n_question = n_question + 1
+             IF (pattern(p:p) == '*') n_asterisk = n_asterisk + 1
+             p = p + 1
+          ENDDO
+          IF (n_question>0) THEN ! first, all the question marks
+             pattern2(p2:p2+n_question-1) = REPEAT('?',n_question)
+             p2 = p2 + n_question
+          ENDIF
+          IF (n_asterisk>0) THEN ! next, the asterisk (only one!)
+             pattern2(p2:p2) = '*'
+             p2 = p2 + 1
+          ENDIF
+       ELSE
+          ! just a normal character
+          pattern2(p2:p2) = pattern(p:p)
+          p2 = p2 + 1
+          p = p + 1
+       ENDIF
+       IF (p > lenp) EXIT
+    ENDDO
+    !!   lenp2 = p2 - 1
+    lenp2 = len_trim(pattern2)
+
+    ! The modified wildcard in pattern2 is compared to the string:
+
+    p2 = 1
+    s = 1
+    match_wild = .FALSE.
+    DO
+       IF (pattern2(p2:p2) == '?') THEN
+          ! accept any char in string
+          p2 = p2 + 1
+          s = s + 1
+
+       ELSEIF (pattern2(p2:p2) == "*") THEN
+          p2 = p2 + 1
+          IF (p2 > lenp2) THEN
+             ! anything goes in rest of string
+             match_wild = .TRUE.
+             EXIT ! .TRUE.
+          ELSE
+             ! search string for char at p2
+             n = INDEX(string(s:), pattern2(p2:p2))
+             IF (n == 0) EXIT  ! .FALSE.
+             s = n + s - 1
+          ENDIF
+
+       ELSEIF (pattern2(p2:p2) == string(s:s)) THEN
+          ! single char match
+          p2 = p2 + 1
+          s = s + 1
+       ELSE
+          ! non-match
+          !       EXIT ! .FALSE.
+          ! Previous line buggy because failure to match one character in the pattern
+          ! does not mean that a match won't be found later. Back up through pattern string
+          ! until first wildcard character is found and start over with the exact character
+          ! match. If the end of the string is reached, then return .FALSE.
+          !      04/11/2011 Robert McClanahan    Robert.McClanahan   <<at>>   AECC.COM
+          !
+          found = .FALSE.
+          DO WHILE (p2 > 0 .AND. .NOT. found)
+             p2 = p2 - 1
+             IF (p2 == 0) EXIT  !  .FALSE.
+             IF (pattern(p2:p2) == '*' .OR. pattern(p2:p2) == '?') found = .TRUE.
+          END DO
+          s = s + 1
+       ENDIF
+
+       IF (p2 > lenp2 .AND. s > lens) THEN
+          ! end of both pattern2 and string
+          match_wild = .TRUE.
+          EXIT ! .TRUE.
+       ENDIF
+
+       IF (s > lens .AND. p2 == lenp) THEN
+          IF(pattern2(p2:p2) == "*") THEN
+             ! "*" at end of pattern2 represents an empty string
+             match_wild = .TRUE.
+             EXIT
+          ENDIF
+       ENDIF
+
+       IF (p2 > lenp2 .OR. s > lens) THEN
+          ! end of either pattern2 or string
+          EXIT ! .FALSE.
+       ENDIF
+    ENDDO
+
+  END FUNCTION match_wild
+
+
+end module strings_module
+
+
+
+
+
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !! Adaption of incom using newer Fortran ( M.M.)
 !! This is to
@@ -288,6 +615,7 @@ MODULE new_com
   common/sig/isignal
 
   character(cmd_len) :: tabformat
+  logical            :: next_is_error 
 
   ! constants
 
@@ -466,7 +794,7 @@ CONTAINS
   !                                                                      *
   !***********************************************************************
   subroutine get_newcom( cmd , istatus)
-
+    use strings_module
     implicit none
     !
     character(*) :: cmd
@@ -1248,6 +1576,7 @@ CONTAINS
          write(99,'(a)')trim(cmd_item(i+2))
          write(99,'(a)')" \\ \hline" 
          close(99)
+         next_is_error = .false.
          goto 8888
        endif
 
@@ -1274,20 +1603,43 @@ CONTAINS
              tabformat = "("//trim(cmd_item(i)(3:))//")"
              cycle
            endif
-           
+
+           if(cmd_item(i)(1:2)=="E:") then
+              cycle
+           endif
+ 
+           if(cmd_item(i+1)(1:2)=="E:") then
+             next_is_error = .true.
+           endif
+          
            
            call evaluate(trim(cmd_item(i))//' ',ev_val,ev_err)
            
            if(ev_err==0) then 
-              write(99,tabformat, advance="no",iostat=ios) ev_val
+              if(abs(ev_val) < 10*epsilon(ev_val)) then
+                 write(99,'(a)',advance="no") "   "       !! ????  oder 0 (mit errorhandlng verknüft)
+              else 
+                 write(99,tabformat, advance="no",iostat=ios) ev_val
+              endif
               if(ios .ne. 0) then
                 write(*,*)"ERROR: tabentrty writing failed: ",trim(cmd_item(i))," F:",trim(tabformat)
                 write(99,'(a)', advance="no")" ??????? "
               endif
            else
+              cmd_item(i) =  string_replace(cmd_item(i),"_","\_")
+              cmd_item(i) =  string_replace(cmd_item(i),"%","\%")
               write(99,'(a)', advance="no") cmd_item(i)(1:max(16,len_trim(cmd_item(i))))
            endif
-           if(i<iitems) write(99,'(a)', advance="no") " & "
+           if(i<iitems) then
+              if(next_is_error) then
+                call evaluate(trim(cmd_item(i+2))//' ',ev_val,ev_err)
+                if(abs(ev_val) >= 10*epsilon(ev_val)) write(99,'(a)', advance="no") " $\pm$"   !! ?? see obe 0
+                next_is_error = .false.
+              else
+                write(99,'(a)', advance="no") " & "
+              endif
+           endif
+     
          enddo
          write(99,'(a)') " \\ \hline"
          close(99)
